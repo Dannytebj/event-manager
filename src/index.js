6d0e644ca65614920048cc59a3f075d795187f4e
Dannytebj <Dannytebj@gmail.com>
2018-05-21 07:16:09 +0100

    Setup React and Webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+
+const LandingPage = () => (
+<div>Welcome</div>
+);
+
+ReactDOM.render(<LandingPage/>, document.getElementById("root"));
